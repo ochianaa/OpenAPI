d7340aefa1984b58095cf91c5325646b6c09446e
ochianaa <gekina01@gmail.com>
2025-03-19 21:44:08 +0800

    membuat HTTP server sederhana dengan node:http

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("Hello world");
+import http from 'http';
+
+const server = http.createServer((req, res) => { 
+    res.writeHead(200, {'Conten-Type': 'text/plain'});
+    res.end('Hello, World!');
+});
+server.listen(3000, () => console.log('Server berjalan di http://localhost:3000'));
